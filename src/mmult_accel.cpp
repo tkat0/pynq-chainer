7ce5adc691e07dc57c73b03d6a55a7f845eb457d
tkato <tkato.dev@gmail.com>
2017-02-17 05:53:27 +0900

    キャッシュ操作のカウンタのリセット忘れ

--- src/mmult_accel.cpp
+++ src/mmult_accel.cpp
@@ -11,84 +11,84 @@ float x_row_cache[CACHE_SIZE];
 float w_row_cache[CACHE_SIZE];
 float y_col_cache[CACHE_SIZE];
 
+static int r_xcnt = 0;
+static int n_xread = 0;
+static int n_xread_pix = 0;
 float* x_get_next_line_offset(float* input, int width, int height) {
-	static int cnt = 0;
-	static int n_read = 0;
-	static int n_read_pix = 0;
 	int n_cache_line = CACHE_SIZE / width;
 
 	if (n_cache_line > height) {
 		n_cache_line = height;
 	}
 
-	debug("[%s]cnt:%d, cache_size: %d, width: %d, height: %d, n_cache_line:%d, n_read:%d\n", __func__, cnt, CACHE_SIZE, width, height, n_cache_line, n_read);
-	if (cnt == 0) {
-		debug("[%s]read from DRAM %d\n", __func__, n_read*n_cache_line*width);
-		memcpy(x_row_cache, &input[n_read * n_cache_line * width], n_cache_line * width * sizeof(float));
-		n_read++;
-		n_read_pix += n_cache_line * width;
-		if (n_read_pix == width*height) {
+	debug("[%s]r_xcnt:%d, cache_size: %d, width: %d, height: %d, n_cache_line:%d, n_xread:%d\n", __func__, r_xcnt, CACHE_SIZE, width, height, n_cache_line, n_xread);
+	if (r_xcnt == 0) {
+		debug("[%s]read from DRAM %d\n", __func__, n_xread*n_cache_line*width);
+		memcpy(x_row_cache, &input[n_xread * n_cache_line * width], n_cache_line * width * sizeof(float));
+		n_xread++;
+		n_xread_pix += n_cache_line * width;
+		if (n_xread_pix == width*height) {
 		    debug("[%s] read all pixels\n", __func__);
-			n_read = 0;
-			n_read_pix = 0;
+			n_xread = 0;
+			n_xread_pix = 0;
 		}
 	}
 
-	int idx = cnt;
-	if (cnt == n_cache_line - 1) {
-		cnt = 0;
+	int idx = r_xcnt;
+	if (r_xcnt == n_cache_line - 1) {
+		r_xcnt = 0;
 	} else {
-		cnt++;
+		r_xcnt++;
 	}
 	debug("[%s] offset:%d\n", __func__, idx*width);
 	return &x_row_cache[idx * width];
 }
 
+static int r_wcnt = 0;
+static int n_wread = 0;
+static int n_wread_pix = 0;
 float* w_get_next_line_offset(float* input, int width, int height) {
-	static int cnt = 0;
-	static int n_read = 0;
-	static int n_read_pix = 0;
 	int n_cache_line = CACHE_SIZE / width;
 
 	if (n_cache_line > height) {
 		n_cache_line = height;
 	}
 
-	debug("[%s]cnt:%d, cache_size: %d, width: %d, height: %d, n_cache_line:%d, n_read:%d\n", __func__, cnt, CACHE_SIZE, width, height, n_cache_line, n_read);
-	if (cnt == 0) {
-		memcpy(w_row_cache, &input[n_read * n_cache_line * width], n_cache_line * width * sizeof(float));
-		n_read++;
-		n_read_pix += n_cache_line * width;
-		if (n_read_pix == width*height) {
-			n_read = 0;
-			n_read_pix = 0;
+	debug("[%s]r_wcnt:%d, cache_size: %d, width: %d, height: %d, n_cache_line:%d, n_wread:%d\n", __func__, r_wcnt, CACHE_SIZE, width, height, n_cache_line, n_wread);
+	if (r_wcnt == 0) {
+		memcpy(w_row_cache, &input[n_wread * n_cache_line * width], n_cache_line * width * sizeof(float));
+		n_wread++;
+		n_wread_pix += n_cache_line * width;
+		if (n_wread_pix == width*height) {
+			n_wread = 0;
+			n_wread_pix = 0;
 		}
 	}
 
-	int idx = cnt;
-	if (cnt == n_cache_line - 1) {
-		cnt = 0;
+	int idx = r_wcnt;
+	if (r_wcnt == n_cache_line - 1) {
+		r_wcnt = 0;
 	} else {
-		cnt++;
+		r_wcnt++;
 	}
 	return &w_row_cache[idx * width];
 }
 
+static int w_cnt = 0;
+static int n_write = 0;
+static int n_write_pix = 0;
 void y_write_cahce(float* output, int width, int height, float data) {
-	static int cnt = 0;
-	static int n_write = 0;
-	static int n_write_pix = 0;
 	int n_cache_line = CACHE_SIZE / width;
 
 	if (n_cache_line > height) {
 		n_cache_line = height;
 	}
 
-	debug("[%s] cnt:%d, width:%d, height:%d, n_cache_line:%d, n_write:%d, data:%f\n", __func__, cnt, width, height, n_cache_line, n_write, data);
-	y_col_cache[cnt] = data;
+	debug("[%s] w_cnt:%d, width:%d, height:%d, n_cache_line:%d, n_write:%d, data:%f\n", __func__, w_cnt, width, height, n_cache_line, n_write, data);
+	y_col_cache[w_cnt] = data;
 
 	// if cache is full then write to DRAM.
-	if (cnt == width*n_cache_line - 1) {
+	if (w_cnt == width*n_cache_line - 1) {
 		// write n lines
 		debug("[%s] write to DRAM offset:%d. size:%d\n",__func__ , n_write*width, width);
 		memcpy(&output[n_write*width*n_cache_line], y_col_cache, width * n_cache_line * sizeof(float));
@@ -98,9 +98,9 @@ void y_write_cahce(float* output, int width, int height, float data) {
 			n_write = 0;
 			n_write_pix = 0;
 		}
-		cnt = 0;
+		w_cnt = 0;
 	} else {
-		cnt++;
+		w_cnt++;
 	}
 }
 
@@ -116,7 +116,16 @@ int mmult_accel1(float *x, float *w, float *y, int x_nrows, int w_nrows, int xw_
 	float *x_row_cache_;
 	float *w_row_cache_;
 	
-	debug("[%s] x: (%d, %d), w: (%d, %d)\n",__func__ , x_nrows, xw_ncols, w_nrows, xw_ncols);
+	debug("[%s] x: (%d, %d), w: (%d, %d) => y: (%d, %d)\n",__func__ , x_nrows, xw_ncols, w_nrows, xw_ncols, x_nrows, w_nrows);
+	r_xcnt = 0;
+	n_xread = 0;
+	n_xread_pix = 0;
+	r_wcnt = 0;
+	n_wread = 0;
+	n_wread_pix = 0;
+	w_cnt = 0;
+	n_write = 0;
+	n_write_pix = 0;
 
 	for (int w_row = 0; w_row < w_nrows; w_row++) {
 
@@ -134,12 +143,14 @@ int mmult_accel1(float *x, float *w, float *y, int x_nrows, int w_nrows, int xw_
 
 			float result = 0.0;
 			for (int k = 0; k < xw_ncols; k++) {
+				//debug("[%s] %d x:%f, w:%f\n", __func__, k, x_row_cache_[k], w_row_cache_[k]);
 				result += x_row_cache_[k] * w_row_cache_[k];
 				//result += in_A[row * a_ncols + k] * in_B[k * b_ncols + col];
 			}
 			//y_col_cache[x_row] = result;
 			debug("[%s] result:%f\n", __func__, result);
-			y_write_cahce(y, x_nrows, w_nrows, result);
+			y_write_cahce(y, w_nrows, x_nrows, result);
+			//y_write_cahce(y, x_nrows, w_nrows, result);
 			////y_write_cahce(y, xw_ncols, x_nrows, result);
 		}
 		// write 1 col to DRAM
