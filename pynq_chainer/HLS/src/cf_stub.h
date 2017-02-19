dc1fbc33251f03eec609e0d1e1d79b06049c4a17
tkato <tkato.dev@gmail.com>
2017-02-19 09:11:16 +0900

    add HLS

---
+++ pynq_chainer/HLS/src/cf_stub.h
@@ -0,0 +1,21 @@
+#ifndef STUBS_H_
+#define STUBS_H_
+
+#include "sds_incl.h"
+#include "portinfo.h"
+#include "accel_info.h"
+#include "sds_lib.h"
+
+extern cf_request_handle_t _p0_request_0;
+extern cf_request_handle_t _p0_request_1;
+extern cf_request_handle_t _p0_request_2;
+extern cf_request_handle_t _p0_request_3;
+extern cf_request_handle_t _p0_request_4;
+extern cf_request_handle_t _p0_request_5;
+extern cf_request_handle_t _p0_request_6;
+
+extern size_t _p0_mmult_accel1_0_num_ap_return;
+
+
+
+#endif
