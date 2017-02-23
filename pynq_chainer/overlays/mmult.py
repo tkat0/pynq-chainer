f0e24eb98b6e0f2669d05cd00acbd5f544f2c90d
tkato <tkato.dev@gmail.com>
2017-02-23 15:59:49 +0000

    wip

--- pynq_chainer/overlays/mmult.py
+++ pynq_chainer/overlays/mmult.py
@@ -16,13 +16,16 @@ class Mmult():
         if IS_PYNQ:
             bitfile = "../pynq_chainer/HLS/bitstream.bit"
             libfile = "../pynq_chainer/HLS/src/libaccel.so"
-            self.ffi.cdef("int _Z18_p0_mmult_accel1_0PfS_S_iii(float*, float*, float*, int, int, int);")
+            #self.ffi.cdef("int _Z18_p0_mmult_accel1_0PfS_S_iii(float*, float*, float*, int, int, int);")
+            self.ffi.cdef("int _Z17_p0_mmult_accel_0PfS_S_iii(float*, float*, float*, int, int, int);")
             self.lib = self.ffi.dlopen(libfile)
-            self.accel_fn = self.lib._Z18_p0_mmult_accel1_0PfS_S_iii
+            #self.accel_fn = self.lib._Z18_p0_mmult_accel1_0PfS_S_iii
+            self.accel_fn = self.lib._Z17_p0_mmult_accel_0PfS_S_iii
             
             overlay = Overlay(bitfile)
             if not overlay.is_loaded():
                 overlay.download()
+                print("load Overlay")
         else:
             self.accel_fn = pcsim.mmult_accel
     
--- pynq_chainer/utils.py
+++ pynq_chainer/utils.py
@@ -3,6 +3,7 @@
 import numpy as np
 import cffi
 
+ffi = cffi.FFI()
 try:
     from pynq.drivers import xlnk
     IS_PYNQ = True
@@ -10,8 +11,9 @@ try:
     libxlnk = ffi.dlopen("/usr/lib/libsds_lib.so")
 except:
     IS_PYNQ = False
+    print("PYNQ FALSE")
 
-ffi = cffi.FFI()
+print("PYNQ", IS_PYNQ)
 
 ffi.cdef("""
 void xlnkFlushCache(void *buf, int size);
@@ -24,9 +26,11 @@ def malloc_cma_ndarray(shape, cacheable=1, dtype="float"):
         buf = memmanager.cma_alloc(length, cacheable=cacheable, data_type=dtype)
         v_cdata = ffi.buffer(buf,  length * ffi.sizeof(dtype))
         v = np.frombuffer(v_cdata, dtype=np.float32).reshape(shape)
+        print("cma alloc")
     else:
         v = np.zeros(shape).astype(np.float32)
         buf = ffi.from_buffer(v.data)
+        print("cma alloc dumy")
     return v, buf
 
 def copy_cma_ndarray(array):
@@ -34,8 +38,10 @@ def copy_cma_ndarray(array):
     array = ffi.cast("float*", array.ctypes.data)
     if IS_PYNQ:
         memmanager.cma_memcopy(cdata, array, 4*x.size)
+        print("cma copy ")
     else:
         ffi.memmove(cdata, array, 4*x.size)
+        print("cma copy dumy")
     return x, cdata
 
 def flush_cache(buf, size):
