a9f81c7ba4c4378874a282f120d01b7b85e8e4c9
tkato <tkato.dev@gmail.com>
2017-03-01 07:46:50 +0900

    hsl

--- src/mmult_accel.cpp
+++ src/mmult_accel.cpp
@@ -7,8 +7,8 @@ void mmult_kernel(inter_t in_A[A_NROWS][A_NCOLS],
 		inter_t in_B[A_NCOLS][B_NCOLS], outer_t* out_C, int a_nrows,
 		int b_ncols, int a_ncols) {
 #pragma HLS INLINE self
-#pragma HLS array_partition variable=in_A block factor=2 dim=2
-#pragma HLS array_partition variable=in_B block factor=2 dim=1
+#pragma HLS array_partition variable=in_A block factor=16 dim=2
+#pragma HLS array_partition variable=in_B block factor=16 dim=1
 
 	int index_a, index_b, index_d;
 
@@ -18,7 +18,7 @@ void mmult_kernel(inter_t in_A[A_NROWS][A_NCOLS],
 		for (index_b = 0; index_b < B_NCOLS; index_b++) {
 #pragma HLS PIPELINE II=1
 //#pragma HLS unroll factor = 32
-			if (index_b > b_ncols - 1) {
+//			if (index_b > b_ncols - 1) {
 				ap_uint<16> result = 0;
 				//#pragma HLS RESOURCE variable=result core=FAddSub_fulldsp
 				for (index_d = 0; index_d < A_NCOLS; index_d++) {
@@ -35,7 +35,7 @@ void mmult_kernel(inter_t in_A[A_NROWS][A_NCOLS],
 
 				}
 				out_C[index_a * B_NCOLS + index_b] = (outer_t) result;
-			}
+//			}
 
 		}
 	}
@@ -50,6 +50,13 @@ void mmult_accel(outer_t* in_A, outer_t* in_B, outer_t* out_C, int a_nrows,
 	int i, j;
 	inter_t a_buf[A_NROWS][A_NCOLS];
 	inter_t b_buf[A_NCOLS][B_NCOLS];
+	for (i = 0; i < A_NROWS; i++) {
+		for (j = 0; j < A_NCOLS; j++) {
+#pragma HLS PIPELINE II=1
+			a_buf[i][j] = 1;
+		}
+	}
+
 //#pragma HLS RESOURCE variable=a_buf core=RAM_2P_BRAM
 //#pragma HLS RESOURCE variable=a_buf core=RAM_2P_BRAM
 
