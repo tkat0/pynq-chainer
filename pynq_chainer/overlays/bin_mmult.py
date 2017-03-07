29af02c5eeba1d2db03c7fac93e230689a63b757
tkato <tkato.dev@gmail.com>
2017-03-08 04:24:20 +0900

    support

--- pynq_chainer/overlays/bin_mmult.py
+++ pynq_chainer/overlays/bin_mmult.py
@@ -17,11 +17,13 @@ class BinMmult():
             bitfile = "../pynq_chainer/HLS/bitstream.bit"
             libfile = "../pynq_chainer/HLS/src/libaccel.so"
             #self.ffi.cdef("void _Z17_p0_mmult_accel_0PiS_S_ii(int*, int*, int*, int, int);")
-            self.ffi.cdef("void _Z20_p0_binary_connect_0tPiS_S_tt(unsigned short, int*, int*, int*, unsigned short, unsigned short);")
+            #self.ffi.cdef("void _Z20_p0_binary_connect_0tPiS_S_tt(unsigned short, int*, int*, int*, unsigned short, unsigned short);")
+            self.ffi.cdef("void _Z17_p0_BlackBoxJam_0P7ap_uintILi32EES1_bjjjS0_(unsigned int*, unsigned int*, bool, unsigned int, unsigned int, unsigned int, unsigned int);")
             self.lib = self.ffi.dlopen(libfile)
             print(self.lib.__dict__)
             #self.accel_fn = self.lib._Z17_p0_mmult_accel_0PiS_S_ii
-            self.accel_fn = self.lib._Z20_p0_binary_connect_0tPiS_S_tt
+            #self.accel_fn = self.lib._Z20_p0_binary_connect_0tPiS_S_tt
+            self.accel_fn = self.lib._Z17_p0_BlackBoxJam_0P7ap_uintILi32EES1_bjjjS0_
             
             overlay = Overlay(bitfile)
             if not overlay.is_loaded():
@@ -30,5 +32,7 @@ class BinMmult():
         else:
             self.accel_fn = pcsim.mmult_accel
     
-    def __call__(self, op, x_cdata, w_cdata, y_cdata, w_nrows, xw_ncols):
-        self.accel_fn(op, x_cdata, w_cdata, y_cdata, w_nrows, xw_ncols)
+    #def __call__(self, op, x_cdata, w_cdata, y_cdata, w_nrows, xw_ncols):
+    #    self.accel_fn(op, x_cdata, w_cdata, y_cdata, w_nrows, xw_ncols)
+    def __call__(self, in, out, init, target_layer, target_mem, target_ind, val):
+        self.accel_fn(in, out, init, target_layer, target_mem, target_ind, val)
